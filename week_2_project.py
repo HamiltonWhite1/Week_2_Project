87ed4de849444f0e01f519242a93361ef1152b64
HamiltonWhite1 <92882456+HamiltonWhite1@users.noreply.github.com>
2022-01-28 14:25:13 -0500

    First commit with the format for Shakti and Hamilton's week 2 project.

---
+++ week_2_project.py
@@ -0,0 +1,15 @@
+
+
+# Parking Garage Class
+class Parking_Garage:
+    def __init__(self, tickets_available, parking_spaces, current_ticket ):
+        self.tickets_available = tickets_available
+        self.parking_spaces = parking_spaces
+        self.current_ticket = current_ticket
+        
+    def take_ticket():
+
+    def payForParking():
+
+    def leaveGarage():
+
